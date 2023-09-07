7df73c28f7f2414cf164f327231c32553165bde0
Sermadl <Sermadl@github.com>
2023-09-07 16:05:26 +0900

    git test

---
+++ gcc_practice/main2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "test1.h"
+#include "test2.h"
+
+int main() {
+
+	printf("Hello, I am Main\n");
+
+	print_function1();
+	print_function2();
+
+	printf("Bye!\n");
+	
+	return 0;
+}
